9598c336255078a08e33baff0779bead3bb24041
adamskipiotr <32665409+adamskipiotr@users.noreply.github.com>
2023-07-08 14:48:20 +0200

    Fix Testcontainers  (#1)
    * Add testcontainer test ste
    
    * Update gradle.yml
    
    * Update gradle.yml
    
    * Update gradle.yml
    
    * Update gradle.yml
    
    * Fix testcontainers
    
    * Change gradle workflow to gradle build
    
    * Add dependency
    
    * add --scan flag
    
    * fix testcontainers config
    
    * github config test
    
    * github config test
    
    ---------
    
    Co-authored-by: adamskipiotr <piotr.adamski@softiq.pl>

--- src/test/java/com/pada/learnproject/LearnprojectApplicationTests.java
+++ src/test/java/com/pada/learnproject/LearnprojectApplicationTests.java
@@ -1,15 +1,45 @@
 package com.pada.learnproject;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@Testcontainers
+@DirtiesContext
+@ContextConfiguration(initializers = LearnprojectApplicationTests.TestEnvInitializer.class)
+@TestPropertySource(properties = {
+    "spring.datasource.driver-class-name="
+})
 public class LearnprojectApplicationTests {
 
+    @Container
+    private static final PostgreSQLContainer<?> POSTGRES_DB = new PostgreSQLContainer<>("postgres:12.8")
+        .withDatabaseName("testdb")
+        .withUsername("postgres")
+        .withPassword("postgres");
+
     @Test
-    void contextLoads() {
+    void testFakeData() {
     }
 
+    static class TestEnvInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            TestPropertyValues values = TestPropertyValues.of(
+                "spring.datasource.url=" + POSTGRES_DB.getJdbcUrl(),
+                "spring.datasource.password=" + POSTGRES_DB.getPassword(),
+                "spring.datasource.username=" + POSTGRES_DB.getUsername()
+            );
+            values.applyTo(applicationContext);
+        }
+    }
+    
 }
