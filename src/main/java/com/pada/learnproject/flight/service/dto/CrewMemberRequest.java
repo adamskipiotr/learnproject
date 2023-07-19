a1380702da46b1cda6d44cc99df8371992bc1e7d
adamskipiotr <32665409+adamskipiotr@users.noreply.github.com>
2023-07-19 23:09:19 +0200

    run new pipeline

--- src/main/java/com/pada/learnproject/flight/service/dto/CrewMemberRequest.java
+++ src/main/java/com/pada/learnproject/flight/service/dto/CrewMemberRequest.java
@@ -2,6 +2,7 @@ package com.pada.learnproject.flight.service.dto;
 
 import com.pada.learnproject.flight.domain.CrewMemberRank;
 
+
 public record CrewMemberRequest(
     String firstName,
     String lastName,
