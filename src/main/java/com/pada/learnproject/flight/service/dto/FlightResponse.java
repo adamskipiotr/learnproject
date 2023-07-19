027e48f12d41d351fcd3ccb587f4e16ebc1ebdb6
adamskipiotr <32665409+adamskipiotr@users.noreply.github.com>
2023-07-19 20:13:28 +0200

    Change responses to records

--- src/main/java/com/pada/learnproject/flight/service/dto/FlightResponse.java
+++ src/main/java/com/pada/learnproject/flight/service/dto/FlightResponse.java
@@ -2,19 +2,10 @@ package com.pada.learnproject.flight.service.dto;
 
 import com.pada.learnproject.flight.domain.FlightStatus;
 import java.time.LocalDateTime;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-public class FlightResponse {
-
-    private LocalDateTime flightStart;
-
-    private LocalDateTime flightEnd;
-
-    private FlightStatus flightStatus;
-
+public record FlightResponse(
+    LocalDateTime flightStart,
+    LocalDateTime flightEnd,
+    FlightStatus flightStatus
+) {
 }
