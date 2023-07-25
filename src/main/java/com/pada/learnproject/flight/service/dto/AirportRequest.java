6ab6fd594eeb02f034a70850d1f4cebc3fb37841
adamskipiotr <32665409+adamskipiotr@users.noreply.github.com>
2023-07-25 18:13:46 +0200

    Fix name in Request

--- src/main/java/com/pada/learnproject/flight/service/dto/AirportRequest.java
+++ src/main/java/com/pada/learnproject/flight/service/dto/AirportRequest.java
@@ -2,5 +2,5 @@ package com.pada.learnproject.flight.service.dto;
 
 import com.pada.learnproject.flight.domain.WeatherCondition;
 
-public record AirportRequest(String airportName, WeatherCondition weatherCondition) {
+public record AirportRequest(String name, WeatherCondition weatherCondition) {
 }
