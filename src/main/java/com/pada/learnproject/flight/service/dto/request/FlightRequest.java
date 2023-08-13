1c28ad2ded93a6396d894ab1dbef4940615b4f8e
adamskipiotr <32665409+adamskipiotr@users.noreply.github.com>
2023-08-13 12:51:12 +0200

    Update FlightRequest.java

--- src/main/java/com/pada/learnproject/flight/service/dto/request/FlightRequest.java
+++ src/main/java/com/pada/learnproject/flight/service/dto/request/FlightRequest.java
@@ -2,5 +2,6 @@ package com.pada.learnproject.flight.service.dto.request;
 
 import java.time.LocalDateTime;
 
-public record FlightRequest(String flightName, LocalDateTime flightStart, LocalDateTime flightEnd, Integer maxPassengerCount) {
+public record FlightRequest(String flightName, LocalDateTime flightStart,
+                            LocalDateTime flightEnd, Integer maxPassengerCount) {
 }
